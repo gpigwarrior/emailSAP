952ad08f405fb22fbeadc46eeff2c63a80da185f
gpiwarr <cgblue99@gmail.com>
2026-03-06 11:44:31 -0500

    for droid

--- signIn.sh
+++ signIn.sh
@@ -9,6 +9,6 @@ awk -k -v awkName="$currentName" '$1==awkName {print $2}' emailList.csv
 # awk -k -v awkName="$currentName" '$1==awkName {print $1 "," $2}' emailList.csv >> totalList.csv
 
 	# exitLoop=done
-done | xclip -selection clipboard
+done
 # cat newList.csv | xclip -selection clipboard
 # sort -u totalList.csv | sed 's/done,//'> /tmp/SAP; cat /tmp/SAP > totalList.csv
